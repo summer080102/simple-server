8d3540b984c7fc618c522d0b219869383096bfbc
summer080102 <winter990801@gmail.com>
2024-10-16 23:01:29 +0100

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 8000;
 
 http.createServer(function(reg, res){
     res.writeHead(200,{"Content-Type" : "text/plain"});
-    res.end("byebye World!");
+    res.end("Welcome to 쓔니");
 }).listen(port,function(){
     console.log('Node sever is running on port $ {port}...');
 });
